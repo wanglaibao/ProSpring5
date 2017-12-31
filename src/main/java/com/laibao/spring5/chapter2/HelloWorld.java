b4a6bc0acb6f7ff1d07f29927bb45841d14468dc
jinge <jinge@wacai.com>
2017-12-31 15:32:53 +0800

    add HelloWorld class to chapter2

---
+++ src/main/java/com/laibao/spring5/chapter2/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.laibao.spring5.chapter2;
+
+/**
+ * @author laibao wang
+ * @date 2017-12-31
+ * @version 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
