6057443ce2f2a6f73df08edb6741c69172b4fc7d
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-01-01 14:03:23 +0800

    add java file info

---
+++ chapter1/src/main/java/com/laibao/spring5/chapter1/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.laibao.spring5.chapter1;
+
+/**
+ * @author laibao wang
+ * @date 2018-01-01
+ * @version 1.0
+ */
+public class HelloWorld {
+
+    public  String sayHello() {
+        return "this is the hello world example!";
+    }
+}
