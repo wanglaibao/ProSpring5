8dc0da8ea163a0067137ca7fef9c85bac3885562
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-08-01 11:41:57 +0800

    update pom.xml file info

---
+++ chapter3/src/test/java/com/laibao/spring5/chapter3/DeclareSpringComponentsTest.java
@@ -0,0 +1,23 @@
+package com.laibao.spring5.chapter3;
+
+import com.laibao.spring5.chapter3.render.MessageRender;
+import org.junit.Test;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+/**
+ * @author laibao wang
+ * @date 2018-08-01
+ * @version 1.0
+ */
+public class DeclareSpringComponentsTest {
+
+    @Test
+    public void testSetXXXInjection() {
+        GenericXmlApplicationContext applicationContext = new GenericXmlApplicationContext();
+        applicationContext.load(new String[]{"classpath:META-INF/spring/app-context-config.xml","classpath:META-INF/spring/xml-beanfactory-config.xml"});
+        applicationContext.refresh();
+        MessageRender messageRender = applicationContext.getBean("render", MessageRender.class);
+        messageRender.render();
+        applicationContext.close();
+    }
+}
