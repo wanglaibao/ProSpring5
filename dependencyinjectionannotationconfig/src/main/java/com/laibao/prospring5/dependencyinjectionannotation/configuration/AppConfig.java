d633fdae3d4feef6c1f10137c6e9011f25cb18a0
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-08-06 01:04:27 +0800

    update pom.xml file info

--- dependencyinjectionannotationconfig/src/main/java/com/laibao/prospring5/dependencyinjectionannotation/configuration/AppConfig.java
+++ dependencyinjectionannotationconfig/src/main/java/com/laibao/prospring5/dependencyinjectionannotation/configuration/AppConfig.java
@@ -1,5 +1,6 @@
 package com.laibao.prospring5.dependencyinjectionannotation.configuration;
 
+import com.laibao.prospring5.dependencyinjectionannotation.SimpleAopBeanPostProcessor;
 import com.laibao.prospring5.dependencyinjectionannotation.repository.AccountRepository;
 import com.laibao.prospring5.dependencyinjectionannotation.service.TransferService;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
  * @version 1.0
  */
 @Configuration
-@ComponentScan(basePackageClasses = {AccountRepository.class, TransferService.class})
+@ComponentScan(basePackageClasses = {AccountRepository.class, TransferService.class, SimpleAopBeanPostProcessor.class})
 //@ComponentScan(basePackages = {"com.laibao.prospring5.dependencyinjectionannotation.repository","com.laibao.prospring5.dependencyinjectionannotation.service"})
 public class AppConfig {
 }
